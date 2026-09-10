671bc468834a7b8ccfcb330b7370a1fe959701ab
Oscar Bao <oscarbaoxianhao@gmail.com>
2026-09-10 11:06:21 -0400

    Cleanup

--- assignments/01-Hello-World/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oscarbao/Desktop/DSA/assignments/01-Hello-World
-# Build directory: /home/oscarbao/Desktop/DSA/assignments/01-Hello-World/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TicTacToeTests]=] "/home/oscarbao/Desktop/DSA/assignments/01-Hello-World/build/run_tests")
-set_tests_properties([=[TicTacToeTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/oscarbao/Desktop/DSA/assignments/01-Hello-World/CMakeLists.txt;29;add_test;/home/oscarbao/Desktop/DSA/assignments/01-Hello-World/CMakeLists.txt;0;")
